fff3c45013ce40c27de6e83d1bda1228c21284b4
Dmitry <lmahaon@yandex.ru>
2024-12-14 19:49:50 +0300

    Add routes tests (pytest)

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -1,13 +1,65 @@
 # conftest.py
 import pytest
 
-# Импортируем класс клиента.
+# тестовый клиент
 from django.test.client import Client
 
-# Импортируем модель заметки, чтобы создать экземпляр.
-from notes.models import Note
+from news.models import News, Comment
 
 
+# объект новости в БД
+@pytest.fixture
+def news():
+    news = News.objects.create(
+        title='Заголовок пробной новости',
+        text='Просто текст',
+    )
+    return news
+
+# пользователь-автор
+
+
+@pytest.fixture
+def author(django_user_model):
+    # Используем встроенную фикстуру для модели пользователей django_user_model.
+    return django_user_model.objects.create(username='Автор')
+
+
+# пользователь - не автор
+@pytest.fixture
+def not_author(django_user_model):
+    return django_user_model.objects.create(username='Мимокрокодил')
+
+
+# клиент автора
+@pytest.fixture
+def author_client(author):
+    client = Client()
+    client.force_login(author)  # Логиним автора в клиенте.
+    return client
+
+
+# клиент неавтора
+@pytest.fixture
+def not_author_client(not_author):  # Вызываем фикстуру автора.
+    client = Client()
+    client.force_login(not_author)  # Логиним автора в клиенте.
+    return client
+
+
+# комментарий автора к новости
+@pytest.fixture
+def comment(author, news):
+    comment = Comment.objects.create(
+        news=news,
+        author=author,
+        text='кг/ам'
+    )
+    return comment
+
+
+'''
+# фикстура
 @pytest.fixture
 # Используем встроенную фикстуру для модели пользователей django_user_model.
 def author(django_user_model):
@@ -19,12 +71,7 @@ def not_author(django_user_model):
     return django_user_model.objects.create(username='Автор2')
 
 
-@pytest.fixture
-def author_client(author):  # Вызываем фикстуру автора.
-    # Создаём новый экземпляр клиента, чтобы не менять глобальный.
-    client = Client()
-    client.force_login(author)  # Логиним автора в клиенте.
-    return client
+
 
 
 @pytest.fixture
@@ -34,15 +81,6 @@ def not_author_client(not_author):
     return client
 
 
-@pytest.fixture
-def note(author):
-    note = Note.objects.create(  # Создаём объект заметки.
-        title='Заголовок',
-        text='Текст заметки',
-        slug='note-slug',
-        author=author,
-    )
-    return note
 
 
 @pytest.fixture
@@ -60,3 +98,4 @@ def form_data():
         'text': 'С другим текстом',
         'slug': 'new-slug'
     }
+'''
--- news/pytest_tests/test_routes.py
+++ news/pytest_tests/test_routes.py
@@ -5,69 +5,64 @@ import pytest
 from pytest_django.asserts import assertRedirects
 
 
+# проверки доступности для анонимов
 @pytest.mark.parametrize(
     'name',  # Имя параметра функции.
     # Значения, которые будут передаваться в name.
-    ('notes:home', 'users:login', 'users:logout', 'users:signup')
+    ('news:home', 'users:login', 'users:logout', 'users:signup')
 )
-# Указываем имя изменяемого параметра в сигнатуре теста.
-def test_pages_availability_for_anonymous_user(client, name):
+@pytest.mark.django_db
+def test_availability_for_anonymous_user(client, name):
+    # используем встроенную фикстуру client из плагина pytest-django
     url = reverse(name)  # Получаем ссылку на нужный адрес.
     response = client.get(url)  # Выполняем запрос.
     assert response.status_code == HTTPStatus.OK
 
 
-@pytest.mark.parametrize(
-    'name',
-    ('notes:list', 'notes:add', 'notes:success')
-)
-def test_pages_availability_for_auth_user(not_author_client, name):
-    url = reverse(name)
-    response = not_author_client.get(url)
+# доступность новости для анонимов
+@pytest.mark.django_db
+def test_availability_news(client, news):
+    url = reverse('news:detail', args=(news.pk,))
+    response = client.get(url)
     assert response.status_code == HTTPStatus.OK
 
 
+# доступ к работе с комментарием для автора
 @pytest.mark.parametrize(
-    # parametrized_client - название параметра,
-    # в который будут передаваться фикстуры;
-    # Параметр expected_status - ожидаемый статус ответа.
-    'parametrized_client, expected_status',
-    # В кортеже с кортежами передаём значения для параметров:
-    (
-        (pytest.lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
-        (pytest.lazy_fixture('author_client'), HTTPStatus.OK)
-    ),
+    'urlname',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('news:edit', 'news:delete')
 )
-@pytest.mark.parametrize(
-    'name',
-    ('notes:detail', 'notes:edit', 'notes:delete'),
-)
-def test_pages_availability_for_users(
-        parametrized_client, name, note, expected_status
-):
-    url = reverse(name, args=(note.slug,))
-    # Делаем запрос от имени клиента parametrized_client:
-    response = parametrized_client.get(url)
-    # Ожидаем ответ страницы, указанный в expected_status:
-    assert response.status_code == expected_status
+@pytest.mark.django_db
+def test_comment_author(comment, author_client, urlname):
+    url = reverse(urlname, args=(comment.pk,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
 
 
+# доступ к работе с комментарием для неавтора
 @pytest.mark.parametrize(
-    'name, args',
-    (
-        ('notes:detail', pytest.lazy_fixture('slug_for_args')),
-        ('notes:edit', pytest.lazy_fixture('slug_for_args')),
-        ('notes:delete', pytest.lazy_fixture('slug_for_args')),
-        ('notes:add', None),
-        ('notes:success', None),
-        ('notes:list', None),
-    ),
+    'urlname',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('news:edit', 'news:delete')
 )
-# Передаём в тест анонимный клиент, name проверяемых страниц и args:
-def test_redirects(client, name, args):
+@pytest.mark.django_db
+def test_comment_notauthor(comment, not_author_client, urlname):
+    url = reverse(urlname, args=(comment.pk,))
+    response = not_author_client.get(url)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+# доступ к работе с комментарием для анонима
+@pytest.mark.parametrize(
+    'urlname',  # Имя параметра функции.
+    # Значения, которые будут передаваться в name.
+    ('news:edit', 'news:delete')
+)
+@pytest.mark.django_db
+def test_comment_anonymous(comment, client, urlname):
+    url = reverse(urlname, args=(comment.pk,))
     login_url = reverse('users:login')
-    # Теперь не надо писать никаких if и можно обойтись одним выражением.
-    url = reverse(name, args=args)
     expected_url = f'{login_url}?next={url}'
     response = client.get(url)
     assertRedirects(response, expected_url)
